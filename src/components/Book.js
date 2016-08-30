2793286edd2f2b71a544b2c72cfc87b8c8438716
Jonathan Sevold <jmsevold@gmail.com>
2016-08-30 05:39:56 -0700

    book component

---
+++ src/components/Book.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+
+const Book = ({title, totalPages,dailyGoal,completionDate,startingPage}) => {
+  return(
+    <div>
+      <p>{}</p>
+    </div>
+  );
+}
+
+export default Book;
+
+
+
+title: '',
+            totalPages: 0,
+            dailyGoal: 0,
+            completionDate: '',
+            startingPage: 0
