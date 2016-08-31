969102b1970e2f993497fe4eb8de135f1270dc85
jmsevold <jmsevold@gmail.com>
2016-08-31 00:21:26 +0000

    putting in bootstrap classes

--- src/components/Book.js
+++ src/components/Book.js
@@ -1,20 +1,21 @@
 import React from 'react';
+/*
+show title, image, daily goal, completion date
+percentage completed, and page you should be on by end of day
+*/
 
-
-const Book = ({title, totalPages,dailyGoal,completionDate,startingPage}) => {
+const Book = ({title,dailyGoal,completionDate}) => {
   return(
     <div>
-      <p>{}</p>
+      <p>{title}</p>
+      <p></p>
+      <p>pages per day:{dailyGoal}</p>
+      <p>Completion date:{completionDate}</p>
     </div>
   );
-}
+};
 
 export default Book;
 
 
 
-title: '',
-            totalPages: 0,
-            dailyGoal: 0,
-            completionDate: '',
-            startingPage: 0
--- src/components/BookForm.js
+++ src/components/BookForm.js
@@ -1,6 +1,25 @@
 import React, { Component } from 'react';
 
+/*
+ <div className="form-group">
+    <label>Title:</label>
+    <input type="text" className="form-control" onChange={this.handleTitleChange}/>
+  </div>
+  <div className="form-group">
+    <label>Daily Goal:</label>
+    <input type="text" className="form-control" onChange={this.handleDailyGoalChange}/>
+  </div>
+  <div className="form-group">
+    <label>Starting Page</label>
+    <input type="text" className="form-control" onChange={this.handleStartingPageChange()}/>
+  </div>
+  <div className="form-group">
+    <label>Total Pages</label>
+    <input type="text" className="form-control" onChange={this.handleTotalPageChange()/}>
+  </div>
+  <button className="btn btn-default" onClick={this.handleCheckState}>Submit</button>
 
+*/
 class BookForm extends Component{
   constructor(props,context){
     super(props,context);
@@ -57,11 +76,11 @@ class BookForm extends Component{
   return(
     <div>
     <p>Realistically, how many minutes can you commit to reading per day? Step away from the computer, and see how many pages you can read in that duration. That should be your daily goal.</p>
-    <span className="glyphicon glyphicon-plus"></span>
+    
       Title: <input type="text" onChange={this.handleTitleChange}/><br/>
       Daily Goal: <input type="text" onChange={this.handleDailyGoalChange}/><br/>
       Starting Page: <input type="text" onChange={this.handleStartingPageChange}/><br/>
-      Total length of book: <input type="text" onChange={this.handleTotalPageChange}/><br/>
+      Total Length: <input type="text" onChange={this.handleTotalPageChange}/><br/>
     <button className="btn btn-primary" onClick={this.handleCheckState}>check state</button>
     </div>
   );
@@ -69,3 +88,5 @@ class BookForm extends Component{
 }
 
 export default BookForm;
+
+ 
