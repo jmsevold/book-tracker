b3653d20ba91b2c58e829d8c96f10cc7602642ee
Jonathan Sevold <jmsevold@gmail.com>
2016-09-07 19:41:04 -0700

    hide api key

--- src/utils/googleBooksAPI.js
+++ src/utils/googleBooksAPI.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_KEY = 'AIzaSyDI6ELm8AYM_vLDy3xJNSP6csRkV_x6Mt0';
+const API_KEY = '';
 
 export function getBookInfo(book){
   return axios.get(`https://www.googleapis.com/books/v1/volumes?q=${book}&key=${API_KEY}`)
