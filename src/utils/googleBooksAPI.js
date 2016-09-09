2816fb3667265aaf63ad27bbc6ebfffe3b54823f
Jonathan Sevold <jmsevold@gmail.com>
2016-09-08 21:07:30 -0700

    refactor

--- src/utils/googleBooksAPI.js
+++ src/utils/googleBooksAPI.js
@@ -2,7 +2,8 @@ import axios from 'axios';
 
 
 const formatUserQuery = (str) => {
-  return /\s/ig.test(str) ? str.replace(/\s/ig, "+") : str
+  let query =  /\s/ig.test(str) ? str.replace(/\s/ig, "+") : str
+  return `q=${query}&`;
 }
 
 const checkResponseCode = (res) => {
@@ -10,24 +11,29 @@ const checkResponseCode = (res) => {
     return res.data.items
   }
   else {
-    return {error: "No books we're found"}
+    return {error: "No books were found"}
   }
 }
 
-const getTitlesAndThumbnailFromBooks = (books) => {
-  for(var {volumeInfo: {title, imageLinks} } of books) {
-    return {title: title, thumbnail: imageLinks.thumbnail}
+const extractBookInfo = (books) => {
+  for(var {volumeInfo: {title, imageLinks, pageCount} } of books) {
+    return {title: title, thumbnail: imageLinks.thumbnail, pageCount: pageCount}
   }
 }
 
-export function getBookInfo(book) {
+const constructQuery = (book) => {
   let googleApiUrl = `https://www.googleapis.com/books/v1/volumes?`
-  let userQuery = "q=" + formatUserQuery(book) + "&"
-  const apiKey = ''
+  let userQuery = formatUserQuery(book)
+  const apiKey = 'AIzaSyDI6ELm8AYM_vLDy3xJNSP6csRkV_x6Mt0';
   let params = `maxResults=10&orderBy=relevance&printType=books`
-  let apiKey = `&key=${apiKey}`
-  let newUrl = `${googleApiUrl}${userQuery}${params}${apiKey}`
-  return axios.get(newUrl)
-    .then(res => checkResponseCode(res))
-      .then(books => getTitlesAndThumbnailFromBooks(books))
+  let key = `&key=${apiKey}`
+  let newUrl = `${googleApiUrl}${userQuery}${params}${key}`
+  return newUrl;
+}
+
+export function bookSearch(book) {
+  let url = constructQuery(book);
+  return axios.get(url)
+    .then(res => extractBookInfo(res.data.items))
+    .catch(error => console.log(error));
 }
