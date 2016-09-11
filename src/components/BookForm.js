ac050172921e0f36c0dd7baffcf1ad2cbb478a77
Jonathan Sevold <jmsevold@gmail.com>
2016-09-10 20:10:44 -0700

    redid form

--- src/components/BookForm.js
+++ src/components/BookForm.js
@@ -1,45 +1,13 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-class BookForm extends Component{
-  constructor(props,context){
-    super(props,context);
-    this.state = {
-      book: {
-        title: '',
-        totalPages: 0,
-        dailyGoal: 0,
-        completionDate: '',
-        startingPage: 0
-      }
-    };
-    this.handleInput = this.handleInput.bind(this);
-  }
-
-  handleInput(e, statekey) {
-    const inputValue = e.target.value;
-    this.setState({
-      [statekey]: inputValue
-    })
-  }
-
-  render(){
-    return(
-      <div>
-        <h2>Realistically, how many minutes can you commit to reading per day? Step away from the computer, and see how many pages you can read in that duration. That should be your daily goal.</h2>
-        <div className="container-fluid">
-          <label>Title:</label>
-          <input type="text" className="form-control" onChange={this.handleInput("title")}/>
-          <label>Daily Goal:</label>
-          <input type="text" className="form-control" onChange={this.handleInput("dailyGoal")}/>
-          <label>Starting Page</label>
-          <input type="text" className="form-control" onChange={this.handleInput("startingPage")}/>
-          <label>Total Pages</label>
-          <input type="text" className="form-control" onChange={this.handleInput("totalPages")}/>
-        </div>
-        <button className="btn btn-success" onClick={this.handleCheckState}>Submit</button>
-      </div>
-    );
- }
+const BookForm = (props) =>{
+  return(
+    <div className="col-md-7 col-md-push-3">
+      <p id="sub-headline-text" className="text-center">What book are you reading?</p>
+      <input type="text" placeholder="" className="form-control input-md" onChange={props.handleTextChange}/>
+      <button id="find-book-button" className='btn btn-success' onClick={props.handleButtonClick}>Find Book</button>
+    </div>
+  );
 }
 
 export default BookForm;
