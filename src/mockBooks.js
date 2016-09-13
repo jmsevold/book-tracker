f1d57d8bcfd0afe731942d9c83220e5246714261
jmsevold <jmsevold@gmail.com>
2016-09-13 15:35:51 +0000

    mock books data

---
+++ src/mockBooks.js
@@ -0,0 +1,21 @@
+const books = [
+  {
+    title: "Harry Potter and the Prisoner of Azkaban",
+    pageCount: 448,
+    thumbnail:"http://books.google.com/books/content?id=Sm5AKLXKxHgC&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api"
+  },
+  {
+    title: "Star Wars: Aftermath",
+    pageCount: 432,
+    thumbnail:"http://books.google.com/books/content?id=ujXTCwAAQBAJ&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api"
+  },
+  {
+    title: "Basic Economics",
+    pageCount: 704,
+    thumbnail:"http://books.google.com/books/content?id=oVaNBQAAQBAJ&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api"
+  },
+  
+];
+
+export default books;
+
