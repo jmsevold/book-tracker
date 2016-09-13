15f89cb7b4eb37df6f96277e848caf0b905a2a7f
jmsevold <jmsevold@gmail.com>
2016-09-13 17:44:45 +0000

    add book goal

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './components/App';
-import BookForm from './components/BookForm';
+import React     from 'react';
+import ReactDOM  from 'react-dom';
+import App       from './components/App';
+import BookForm  from './components/BookForm';
 import StartPage from './components/StartPage';
+import BookGoal  from  './components/BookGoal';
 import './index.css';
 import {Router, Route, hashHistory, IndexRoute} from 'react-router'
 
