d666f7555e286a9ee9f439c8ca9f7aa406ef2a95
jmsevold <jmsevold@gmail.com>
2016-09-14 18:19:34 +0000

    App.js in containers folder

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React     from 'react';
 import ReactDOM  from 'react-dom';
-import App       from './components/App';
+import App       from './containers/App';
 import BookForm  from './components/BookForm';
 import StartPage from './components/StartPage';
 import BookGoal  from  './components/BookGoal';
