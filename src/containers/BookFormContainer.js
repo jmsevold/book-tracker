f7def1ffe7b8e51f57ba7c51783dd83d49aa97d3
jmsevold <jmsevold@gmail.com>
2016-09-14 21:07:47 +0000

    fleshing out bookform container

---
+++ src/containers/BookFormContainer.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import * as googleBooksAPI from '../utils/googleBooksAPI';
+import BookForm from '../components/BookForm';
+
+
+
+class BookForm extends React.Component{
+  constructor(props,context){
+    super(props,context);
+    this.state = {
+      book: {},
+      bookQuery: ''
+    };
+    this.handleButtonClick = this.handleButtonClick.bind(this);
+    this.handleTextChange  = this.handleTextChange.bind(this);
+  }
+
+
+  handleButtonClick(){
+    const bookQuery = this.state.bookQuery;
+    googleBooksAPI.bookSearch(bookQuery).then((book) => {
+      this.setState({
+        book
+      });
+    });
+  }
+
+
+
+  handleTextChange(e){
+    const bookQuery = e.target.value;
+    this.setState({
+      bookQuery: bookQuery
+    });
+  }
+
+  render(){
+    return(
+      <BookForm onChange={this.handleTextChange} onclick={this.handleButtonClick}/>
+    );
+  }
+}
+
+
+export default BookForm;
