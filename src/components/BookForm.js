ab65fe628740aa8a8646316e53d04380ddd1b66a
jmsevold <jmsevold@gmail.com>
2016-09-14 21:31:26 +0000

    minor refactor

--- src/components/BookForm.js
+++ src/components/BookForm.js
@@ -2,12 +2,20 @@ import React from 'react';
 
 const BookForm = (props) =>{
   return(
-    <div className="col-md-7 col-md-push-3">
-      <p id="sub-headline-text" className="text-center">What book are you reading?</p>
-      <input type="text" placeholder="" className="form-control input-md" onChange={props.handleTextChange}/>
-      <button id="find-book-button" className='btn btn-success' onClick={props.handleButtonClick}>Find Book</button>
-    </div>
+    <div id="book-container">
+      <div className="container">
+        <div className="row">
+          <div className="col-md-7 col-md-push-3">
+            <p id="sub-headline-text" className="text-center">What book are you reading?</p>
+            <input type="text" placeholder="" className="form-control input-md" onChange={props.handleTextChange}/>
+            <button id="find-book-button" className='btn btn-success' onClick={props.handleButtonClick}>Find Book</button>
+          </div>
+        </div>
+      </div>
+    </div> 
   );
 }
+
+
 
 export default BookForm;
--- src/components/BooksList.js
+++ src/components/BooksList.js
@@ -7,13 +7,7 @@ import Book from './Book';
 const BooksList = ({books, toggleEnableForm}) => {
   return(
     <div className="col-xs-3">
-      {
-        books.map((book, index) => {
-          return(
-            <Book book={book} key={index}/>
-          );
-        })
-      }
+      {books.map((book, index) => <Book book={book} key={index}/>)}
       <a><span className='glyphicon glyphicon-plus' onClick={toggleEnableForm}/></a>
       Add Book
     </div>
